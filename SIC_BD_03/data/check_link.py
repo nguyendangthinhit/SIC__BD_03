02dbccc6fad41646eb9a9878792e66f2f709f241
nguyendangthinhit <nguyendangthinh.it@gmail.com>
2025-07-11 22:06:38 +0700

    cập nhập file process.py

--- SIC_BD_03/data/check_link.py
+++ SIC_BD_03/data/check_link.py
@@ -1,5 +1,5 @@
 def check(link):
-    with open('link.txt', 'r', encoding='utf-8') as f:
+    with open('links.txt', 'r', encoding='utf-8') as f:
         for line in f:
             if link==line.strip():
                 return "trùng rồi"
